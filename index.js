85f8248cf0c222cf8f7510a339b91fe4a2d68e0a
Juanmi <jmcostalucena2000@gmail.com>
2023-05-22 10:16:34 +0200

    Allow Origin Modified

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const dominiosPermitidos = [process.env.FRONTEND_URL];
 
 /** CORS setting with OPTIONS pre-flight handling */
 app.use(function(req, res, next){
-    res.header('Access-Control-Allow-Origin', dominiosPermitidos);
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, accept, access-control-allow-origin');
 
